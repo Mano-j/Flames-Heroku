d8097e7e8754538992935bc751170af193708d3a
Mano-j <manojm.mailid@gmail.com>
2020-01-27 17:16:07 +0530

    Debug=False

--- flames/settings.py
+++ flames/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gy47@!nm$%do%$hg6a(v=6#0szuga+%q7kjo-py#s==c80b%3^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
